877d302097fd9f0a79f4bd32058cef55bca11d16
Kristijan Majkic <81744751+Kristijan-M@users.noreply.github.com>
2022-03-09 20:02:21 +0100

    Welcome text animation

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
 import logo from './profileralogo.png';
-function App() {
-  return (
-    <div>
-      <header>
-      <img src={logo} />
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+import styled, {keyframes} from 'styled-components';
+import { bounce } from 'react-animations';
+
+const Bounce = styled.div`animation: 2s ${keyframes`${bounce}`} infinite`; 
+
+// function App() {
+//   return (
+//     <div>
+//       <header>
+//       <img src={logo} />
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+class ReactAnimations extends Component {
+  render() {
+    return (
+      <div> 
+          
+          <header>
+              <img src={logo} alt = "profilera logo "/>
+          </header>
+          <p className='welcometxt'> <Bounce><h1> Welcome to profilera! </h1></Bounce> </p>
+      </div>
+      
+    );
+  }
 }
 
-export default App;
+export default ReactAnimations
