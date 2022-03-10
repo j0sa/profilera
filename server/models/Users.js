ffe5368dd110c03c9ccefa4a5144de9489e755b0
Jonas Samuelsson <josa@tuta.io>
2022-03-10 10:17:42 +0100

    basic model "done"

---
+++ server/models/Users.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      admin: {
+        type: Boolean,
+        required: true,
+      },
+      analysis: {
+        analysisId: {
+            type: Int16Array,
+            required: true,
+          },
+          date: {
+            type: Date,
+            required: false,
+          },
+          dataset: {
+            type: String,
+            required: false,
+          },
+          response: {
+            type: String,
+            required: false,
+          },
+          status: {
+            type: Int16Array,
+            required: false,
+          },
+      },
+});
+
+UserSchema.pre('save', async function (next) {
+    try {
+        console.log("Running before saving...")
+    } catch (error) {
+        next(error)
+    }
+})
+
+const User = mongoose.model("users", UserSchema);
+module.exports = User;
