595e1c9d1c9f1c79ec5571e25425efb2dc7486f8
Jonas Samuelsson <josa@tuta.io>
2022-03-11 15:03:48 +0100

    ruotes cleaned

--- server/index.js
+++ server/index.js
@@ -5,81 +5,20 @@ require("dotenv").config({
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const userModel = require("./models/Users");
+//const userModel = require("./models/Users");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+//const bcrypt = require("bcrypt");
 
 const port = process.env.PORT || 5000;
 const databaseURI = process.env.DATABASE_URI;
+const userRouter = require("./routes/users");
 
 app.use(express.json());
 app.use(cors());
 
 mongoose.connect(databaseURI);
 
-app.get("/getUsers", (req, res) => {
-  userModel.find({}, (err, result) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json(result);
-    }
-  });
-});
-
-//skapar en användare med hashad lösenord
-app.post("/createUser", async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const user = {
-      name: req.body.name,
-      password: hashedPassword,
-      email: req.body.email,
-      admin: req.body.admin,
-      analysis: req.body.analysis,
-    };
-    const newUser = new userModel(user);
-    await newUser.save();
-    res.json(user);
-  } catch {
-    res.status(500).send();
-  }
-});
-
-app.post("/users/login", async (req, res) => {
-  const userLogin = userModel.find((user) => user.name == req.body.name);
-  console.log(userLogin);
-  if (userLogin == null) {
-    return res.status(400).send("No user with this name...");
-  }
-  try {
-    if (await bcrypt.compare(req.body.password, userLogin.password)) {
-      res.send("Successful!");
-    } else {
-      res.send("Error...");
-    }
-  } catch (error) {
-    res.status(500).send();
-    console.log(error);
-  }
-});
-
-//inloggning kollar om namn och lösenord stämmer TODO
-//  app.post('/users/login', async (req, res) => {
-//     const userL = userModel.find(user => user.name == req.body.name)
-//     if (userL == null) {
-//       return res.status(400).send('Cannot find user')
-//     }
-//     try {
-//       if(await bcrypt.compare(req.body.password, userL.password)) {
-//         res.send('Success')
-//       } else {
-//         res.send('Not Allowed');
-//       }
-//     } catch {
-//       res.status(500).send();
-//     }
-//   });
+app.use("/users", userRouter);
 
 app.listen(port, () => {
   console.log("[Server]::LISTEN:%s", port);
---
+++ server/routes/users.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const router = express.Router();
+const userModel = require("../models/Users");
+
+router.get("/", (req, res) => {
+  userModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+router.post("/users/new", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = {
+      name: req.body.name,
+      password: hashedPassword,
+      email: req.body.email,
+      admin: req.body.admin,
+      analysis: req.body.analysis,
+    };
+    const newUser = new userModel(user);
+    await newUser.save();
+    res.json(user);
+  } catch {
+    res.status(500).send();
+  }
+});
+
+router.post("/users/login", async (req, res) => {
+  const userLogin = userModel.find((user) => user.name == req.body.name);
+  console.log(userLogin);
+  if (userLogin == null) {
+    return res.status(400).send("No user with this name...");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, userLogin.password)) {
+      res.send("Successful!");
+    } else {
+      res.send("Error...");
+    }
+  } catch (error) {
+    res.status(500).send();
+    console.log(error);
+  }
+});
+
+//inloggning kollar om namn och lösenord stämmer TODO
+//  app.post('/users/login', async (req, res) => {
+//     const userL = userModel.find(user => user.name == req.body.name)
+//     if (userL == null) {
+//       return res.status(400).send('Cannot find user')
+//     }
+//     try {
+//       if(await bcrypt.compare(req.body.password, userL.password)) {
+//         res.send('Success')
+//       } else {
+//         res.send('Not Allowed');
+//       }
+//     } catch {
+//       res.status(500).send();
+//     }
+//   });
+
+module.exports = router;
