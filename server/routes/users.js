94cce1e851b9ee7e57855cf7463d1b838488c3b0
Jonas Samuelsson <josa@tuta.io>
2022-03-11 16:23:11 +0100

    delete and findOne done, put kinda weird doe

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 const userModel = require("../models/User");
+const cors = require("cors");
 
 router.get("/", (req, res) => {
   userModel.find({}, (err, result) => {
@@ -13,6 +14,7 @@ router.get("/", (req, res) => {
   });
 });
 
+// EMAIL IS UNIQUE
 router.post("/new", async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -31,7 +33,7 @@ router.post("/new", async (req, res) => {
   }
 });
 
-router.post("/users/login", async (req, res) => {
+router.post("/login", async (req, res) => {
   const userLogin = userModel.find((user) => user.name == req.body.name);
   console.log(userLogin);
   if (userLogin == null) {
@@ -65,5 +67,75 @@ router.post("/users/login", async (req, res) => {
 //       res.status(500).send();
 //     }
 //   });
+
+// router.get("/:id", cors(), async (req, res) => {
+//   var id = req.params.id;
+//   userModel.find({ _id: id }, function (err, users) {
+//     if (err) res.send(err);
+//     //console.log(id);
+//     res.json(users);
+//   });
+// });
+
+// router.get("/delete/:id", cors(), async (req, res) => {
+//   var id = req.params.id;
+//   userModel.findByIdAndDelete({ _id: id }, function (err) {
+//     if (err) return res.send(500, err);
+//     console.log(`User: ${id} was deleted...`);
+//     res.redirect("/users");
+//   });
+// });
+
+router
+  .route("/:id")
+  .get((req, res, next) => {
+    userModel
+      .findOne({
+        _id: req.params.id,
+      })
+      .then((thing) => {
+        res.status(200).json(thing);
+      })
+      .catch((error) => {
+        res.status(404).json({
+          error: error,
+        });
+      });
+  })
+  .put((req, res, next) => {
+    const user = new userModel({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+      admin: req.body.admin,
+      analysis: req.body.analysis,
+    });
+    userModel
+      .updateOne({ _id: req.params.id }, user)
+      .then(() => {
+        res.status(201).json({
+          message: "User updated successfully!",
+        });
+      })
+      .catch((error) => {
+        res.status(400).json({
+          error: error,
+        });
+      });
+  })
+  .delete((req, res, next) => {
+    userModel
+      .deleteOne({ _id: req.params.id }) // deleteOne() is used to delete a single document, findOneAndDelete() returns the deleted document after having deleted it (in case you need its contents after the delete operation)
+      .then(() => {
+        res.status(200).json({
+          message: "User Was deleted!",
+        });
+      })
+      .catch((error) => {
+        res.status(400).json({
+          error: error,
+        });
+      });
+  });
 
 module.exports = router;
