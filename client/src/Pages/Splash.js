159167449e83539f73206faee33c408584020183
Kristijan Majkic <81744751+Kristijan-M@users.noreply.github.com>
2022-03-25 11:22:31 +0100

    Style fixes
    Fixade slide och slide text

--- client/src/Pages/Splash.js
+++ client/src/Pages/Splash.js
@@ -32,10 +32,12 @@ const slideImages = [
   {
     url: "https://media.foretagsforumet.se/2020/09/Skillnaden-mellan-fysisk-och-juridisk-person.jpg",
     caption: "Kristijan Majkic",
+    infotxt: "Delevoper"
   },
   {
     url: "https://avatars.githubusercontent.com/u/47446346?v=4",
     caption: "Jonas Samuelsson",
+    infotxt: "Developer"
   },
 ];
 
@@ -90,17 +92,36 @@ const Splash = () => {
         </Fade>
       </div>
 
-      <div className="meet-the-team-div">
+<div class="meet-the-team-div">
+
+<div class="meet-the-team-text-div">
+        <Fade left>
+        <h2>
+          Meet our team!
+        </h2>
+        <h3>
+          We are a small but dedicated and commited team of developers.
+        </h3>
+
+        </Fade>
+
+      </div>
+
+<div className="meet-the-team-slide-div">
+        
         <Slide>
           {slideImages.map((slideImage, index) => (
             <div className="each-slide" key={index}>
               <div style={{ backgroundImage: `url(${slideImage.url})` }}>
                 <span>{slideImage.caption}</span>
               </div>
+              <p class="info-text-p">{slideImage.infotxt} </p>
             </div>
           ))}
         </Slide>
       </div>
+
+</div>
 
       <div class="CTA-div">
         <Fade up>
