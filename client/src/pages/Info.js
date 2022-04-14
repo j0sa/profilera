e392cd0d4f0eac869d3a40283b32f211f217744f
Kristijan Majkic <81744751+Kristijan-M@users.noreply.github.com>
2022-04-14 09:20:56 +0200

    Added login btn

--- client/src/pages/Info.js
+++ client/src/pages/Info.js
@@ -1,34 +1,42 @@
 import React from "react";
 import "./css/Info.scss";
 import Fade from "react-reveal/Fade";
+import Button from "@mui/material/Button";
 
 const Info = () => {
   return (
-    <div className="info-txt-customer-segmentation-div">
-      <Fade left>
-        <h1> What is customer segmentation and what are personas?</h1>
+    <div>
+      <div className="profile-btn-div">
+        <Button variant="contained" color="secondary" size="large">
+          Log in
+        </Button>
+      </div>
+      <div className="info-txt-customer-segmentation-div">
+        <Fade left>
+          <h1> What is customer segmentation and what are personas?</h1>
 
-        <p>
-          {" "}
-          Customer segmentation is the process by which you divide your customer
-          base based on common characteristics – such as behaviors, income, age,
-          location and interests. This is done in order to market to those
-          customers more effectively.
-        </p>
+          <p>
+            {" "}
+            Customer segmentation is the process by which you divide your
+            customer base based on common characteristics – such as behaviors,
+            income, age, location and interests. This is done in order to market
+            to those customers more effectively.
+          </p>
 
-        <p>
-          The groups that form as a result of customer segmentation can also be
-          used to create a marketing persona. A marketing persona is a fictive
-          person that is made up of the traits that in this case is in common
-          for a group of customers. This is because customer segmentation is
-          typically used to improve how a business sells and so marketing
-          personas need to be closely aligned with these customer segments in
-          order to provide a better understanding of which marketing methods are
-          best suited for the persona. For each group created as a result of
-          customer segmentation a specifikt persona is created to represent each
-          one of these groups.
-        </p>
-      </Fade>
+          <p>
+            The groups that form as a result of customer segmentation can also
+            be used to create a marketing persona. A marketing persona is a
+            fictive person that is made up of the traits that in this case is in
+            common for a group of customers. This is because customer
+            segmentation is typically used to improve how a business sells and
+            so marketing personas need to be closely aligned with these customer
+            segments in order to provide a better understanding of which
+            marketing methods are best suited for the persona. For each group
+            created as a result of customer segmentation a specifikt persona is
+            created to represent each one of these groups.
+          </p>
+        </Fade>
+      </div>
     </div>
   );
 };
